13dc7eca030459a9e7e9ad53adc188753811e5a3
Bela Pecsek <50743731+bpecsek@users.noreply.github.com>
2020-11-01 19:56:02 +0100

    Update fast-trig.lisp

--- fast-trig.lisp
+++ fast-trig.lisp
@@ -1,21 +1,28 @@
 (eval-when (:compile-toplevel :load-toplevel :execute)
   (defmacro def-fast-trig (name extern-alien internal type)
     `(eval-when (:compile-toplevel :load-toplevel :execute)
-       (declaim (inline ,name))
+       (declaim (inline ,name)
+		(ftype (function (,type) ,type) ,name))
        (defun ,name (x)
-	 (sb-kernel::alien-funcall
-	  (sb-kernel::extern-alien ,extern-alien (function ,type (,type))) x))
+	 (declare (optimize (speed 3) (safety 0) (debug 0) (space 0))
+		  (type ,type x))
+	 (sb-c::alien-funcall
+	  (sb-c::extern-alien ,extern-alien (function ,type (,type))) x))
        (sb-c:defknown (,name)
 	   (,type) ,type (sb-c:movable sb-c:foldable sb-c:flushable))
        (sb-c::deftransform ,internal ((x) (,type) *)
 	 '(,name x))))
-  (def-fast-trig %sinf "sin" sin single-float)
-  ;(def-fast-trig %sinf "sinf" sin single-float)
-  (def-fast-trig %sind "sin" sin double-float)
-  (def-fast-trig %cosf "cos" cos single-float)
-  (def-fast-trig %cosd "cos" cos double-float)
-  (def-fast-trig %tanf "tan" tan single-float)
-  (def-fast-trig %tand "tan" tan double-float))
+  (def-fast-trig sinf "sinf" sin single-float)
+  (def-fast-trig cosf "cosf" cos single-float)
+  (def-fast-trig tanf "tanf" tan single-float))
+
+;; (in-package :cl-user)
+;; (declaim (inline sinf)
+;; 	 (ftype (function (single-float) single-float) sinf))
+;; (defun sinf (x)
+;;   (declare (optimize (speed 3) (safety 0) (debug 0))
+;; 	   (type single-float x))
+;;   (sin x))
 
 (defun sin-bench (n)
   (declare (optimize (speed 3) (safety 0) (debug 0)))
@@ -23,4 +30,3 @@
 	      from 0.0 by 0.1 summing (sin x))))
 
 ;(sin-bench 100000000)
-
