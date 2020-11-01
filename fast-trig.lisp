30865709241e43e478421253f89292f5164543c4
bpecsek <bela.pecsek@gmail.com>
2020-11-01 20:25:09 +0100

    01.11.2020

--- fast-trig.lisp
+++ fast-trig.lisp
@@ -3,18 +3,27 @@
     `(eval-when (:compile-toplevel :load-toplevel :execute)
        (declaim (inline ,name))
        (defun ,name (x)
-	 (sb-kernel::alien-funcall
-	  (sb-kernel::extern-alien ,extern-alien
-			      (function ,type (,type))) x))
+	 (sb-c::alien-funcall
+	  (sb-c::extern-alien ,extern-alien (function ,type (,type))) x))
        (sb-c:defknown (,name)
 	   (,type) ,type (sb-c:movable sb-c:foldable sb-c:flushable))
        (sb-c::deftransform ,internal ((x) (,type) *)
 	 '(,name x))))
-  (def-fast-trig %sinf "sin" sin single-float)
-  (def-fast-trig %sind "sin" sin double-float)
-  (def-fast-trig %cosf "cos" cos single-float)
-  (def-fast-trig %cosd "cos" cos double-float)
-  (def-fast-trig %tanf "tan" tan single-float)
-  (def-fast-trig %tand "tan" tan double-float))
+  (def-fast-trig %sinf "sinf" sin single-float)
+  (def-fast-trig %cosf "cosf" cos single-float)
+  (def-fast-trig %tanf "tanf" tan single-float))
 
+(declaim (inline sinf)
+	 (ftype (function (single-float) single-float) sinf))
+(defun sinf (x)
+  (declare (optimize (speed 3) (safety 0) (debug 0))
+	   (type single-float x))
+  (%sinf x))
+
+(defun sin-bench (n)
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (time (loop repeat n for x of-type single-float
+	      from 0.0 by 0.1 summing (sinf x))))
+
+;(sin-bench 100000000)
 
